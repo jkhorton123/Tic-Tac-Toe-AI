33331cb3f562b972bd3d59ccae9011a152a2fc5c
Jack Horton <jkhorton123@gmail.com>
2021-07-31 23:09:09 -0400

    Added separate 2-palayer mode and user vs AI mode

--- T-T-T_Interface.py
+++ T-T-T_Interface.py
@@ -1,3 +1,5 @@
+#from AIFunctions import AI
+
 
 class Board():
     def __init__(self):
@@ -10,14 +12,14 @@ class Board():
         Parameters:
             Tlet (str) : X or O
 
-        Returns the winning letter if a win is detected and "False" if not
+        Returns True if a win for the player with Tlet is detected and False if not
     
         """
         winCases = [(0,1,2),(0,3,6),(0,4,8),(3,4,5),(6,7,8),(1,4,7),(2,5,8),(2,4,6)]
         for case in winCases:
-            if self.boardArr[case[0]] == self.boardArr[case[1]] == self.boardArr[case[2]]:
-                return self.boardArr[case[0]]
-        return "False"
+            if Tlet == self.boardArr[case[0]] == self.boardArr[case[1]] == self.boardArr[case[2]]:
+                return True
+        return False
 
 
     def printBoard(self):
@@ -27,80 +29,140 @@ class Board():
         [({6}),({7}),({8})]
         """.format(*self.boardArr))
 
-    def updateBoard(self, Tlet):
+    def twoPlayerUpdateBoard(self, Tlet, Ordering):
         """
         Updates the T-T-T board and displays it in the terminal
 
-        Parameters 
-        ----------
-        Tlet = str
-            X or O that will be placed on the board
+        Parameters:
+            Tlet (str) : X or O that will be placed on the board
+            Ordering (str) : 1st or 2nd which states which user (or AI) the letter is associated with
+        
+
         """
         locAccepted = False
         while not locAccepted:
-            loc = input("Enter a location number: ")
-            if loc.isdigit():
-                if int(loc) in range(9):
-                    if str(self.boardArr[int(loc)]).isdigit():
-                        self.boardArr[int(loc)] = Tlet
-                        self.printBoard()
-                        locAccepted = True
+            if Ordering == "P1":
+                loc = input("Player 1, please enter a location number: ")
+                if loc.isdigit():
+                    if int(loc) in range(9):
+                        if str(self.boardArr[int(loc)]).isdigit():
+                            self.boardArr[int(loc)] = Tlet
+                            self.printBoard()
+                            locAccepted = True
+            
+            else:
+                loc = input("Player 2, please enter a location number: ")
+                if loc.isdigit():
+                    if int(loc) in range(9):
+                        if str(self.boardArr[int(loc)]).isdigit():
+                            self.boardArr[int(loc)] = Tlet
+                            self.printBoard()
+                            locAccepted = True
+
 
 
 def getUserLetter():
     """
-    Obtains and sets the user's chosen letter (X or O) and the AI's
+    Obtains and sets player 1's chosen letter (X or O) and the other player's (either AI or player 2)
+    Note that X goes first 
     """
     
-    # Set userLet to the user's letter and AILet to the AI's
+    # Set userLet to player 1's letter and otherLet to the AI's or player 2's
     letAccepted = False
 
     while not letAccepted:
-        inputLet = input("Please enter your chosen letter (X or O): ")
+        inputLet = input("Please enter player 1's letter (X or O): ")
         if inputLet.lower() == "x" or inputLet.lower() == "'x'":
-            userLet = "X" 
-            AILet = "O"
+            P1Let = 'X'
+            otherLet = 'O'
             letAccepted = True
 
         if inputLet.lower() == "o" or inputLet.lower() == "'o'":
-            userLet = "O"
-            AILet = "X"
+            P1Let = 'O'
+            otherLet = 'X'
             letAccepted = True
 
-    return userLet, AILet
+    return P1Let, otherLet
 
-def main():
+def checkMode():
     """
-    Initializes and executes the game 
+    Checks and sets the user-selected mode to User Vs AI if 1 is entered and 2-player Mode if 2 is entered
     """
+
+    modeMsg = "Enter 1 for User Vs AI Mode and Enter 2 for 2-Player Mode: "
+    mode = input(modeMsg)
+    while not mode.isdigit():
+        mode = input(modeMsg)
+    modeNum = int(mode)
+    while not modeNum == 1 and not modeNum == 2:
+        mode = input(modeMsg)
+        modeNum = int(mode)
+    return modeNum
+
+def twoPlayerMode():
     gameBoard = Board() 
-    print("Welcome to Tic-Tac-Toe!")
-    userLet, AILet = getUserLetter()
-
+    P1Let, P2Let = getUserLetter()
+    if P1Let == "X":
+        first = "P1"
+        second = "P2"
+    else: 
+        first = "P2"
+        second = "P1"
     gameBoard.printBoard()
 
     gameEnd = False
     plays = 1
     while not gameEnd:
         if plays % 2 == 0:
-            gameBoard.updateBoard(AILet)
+            gameBoard.twoPlayerUpdateBoard('O', second)
         else:
-            gameBoard.updateBoard(userLet)
+            gameBoard.twoPlayerUpdateBoard('X', first)
             
         if plays >= 5:
-            win = gameBoard.checkWin(userLet)
-            if win == userLet:
-                gameEnd = True
-                print("User wins!")
-                return
-            if win == AILet:
-                gameEnd = True
-                print("AI wins!")
-                return
+            if plays % 2 == 0:
+                win = gameBoard.checkWin('O')
+                if win == True:
+                    gameEnd = True
+                    if second == "P1":
+                        print("Player 1 wins!")
+                    else: 
+                        print("Player 2 wins!")
+                    return
+    
+            else: 
+                win = gameBoard.checkWin('X')
+                if win == True:
+                    gameEnd = True
+                    if first == "P1":
+                        print("Player 1 wins!")
+                    else: 
+                        print("Player 2 wins!")
+                    return
+        
         if plays == 9:
             gameEnd = True
             print("It's a tie!")
+            return
         plays += 1
+
+def AIMode():
+    pass
+
+def main():
+    """
+    Initializes and executes the game 
+    """
+    print("Welcome to Tic-Tac-Toe!")
+
+    mode = checkMode()
+    if mode == 1:
+        AIMode()
+    else: 
+        twoPlayerMode()
+
+   #userLet, AILet = getUserLetter()
+
+    
 
 if __name__ == "__main__":
     main()
